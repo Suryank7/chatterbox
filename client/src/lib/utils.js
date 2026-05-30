05d403c5a7f7c347d2c92aeff7deba9254b37357
Suryank Malik <asksuryank@gmail.com>
2026-05-30 22:42:54 +0530

    first commit

---
+++ client/src/lib/utils.js
@@ -0,0 +1,10 @@
+// utility functions
+
+export function formatMessageTime(date) {
+    return new Date(date).toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: "false",
+    })
+}
+ 
