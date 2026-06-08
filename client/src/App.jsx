e67143df7c07a29f2a83dfc4aee1c08489f253b2
Suryank Malik <asksuryank@gmail.com>
2026-06-08 17:01:04 +0530

    added complete server code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
+import { Toaster } from "react-hot-toast"
+// import { AuthContext } from '../context/AuthContext'
+import { AuthContext } from "../context/AuthContext";
+
 
 const App = () => {
+  const {AuthUser}=useContext(AuthContext)
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-cover bg-center min-h-screen">
+    <div className="bg-[url('./bgImage.svg')] bg-cover bg-center min-h-screen">
+      <Toaster />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={AuthUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!AuthUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/profile"
+          element={AuthUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </div>
   );
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import assets from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 
 const LoginPage = () => {
   const [currState, setCurrState] = useState("Sign Up");
@@ -7,7 +8,9 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [bio, setBio] = useState("");
-    const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+  const [isDataSubmitted, setIsDataSubmitted] = useState(false);
+  
+  const {login} = useContext(AuthContext)
     
     const onSubmitHandler = (event) => {
         event.preventDefault();
@@ -16,7 +19,7 @@ const LoginPage = () => {
             setIsDataSubmitted(true)
         return;
         }
-        
+        login(currState==="Sign up"? 'signup':'login',{fullName,email,password,bio})
     }
   return (
     <div className="min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl">
